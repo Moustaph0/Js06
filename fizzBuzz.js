2f534b73c9ded47a68f48fcd51609d0cca7aada2
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-15 12:22:28 +0100

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,10 +1,11 @@
 
 function fizzBuzz() {
-    for (var i = 1; i < 101; i++) {
-        if (i % 15 == 0) console.log("FizzBuzz");
-        else if (i % 3 == 0) console.log("Fizz");
-        else if (i % 5 == 0) console.log("Buzz");
+    for (let i = 1; i <= 100; i++) {
+        else if (i % 3 == 0) {console.log("Fizz");}
+        else if (i % 5 == 0) {console.log("Buzz");}
+        if (i % 3 == 0 && i % 5== 0) {console.log("FizzBuzz");}
         else console.log(i);
     }
 }
-module.exports = fizzBuzz();
+fizzBuzz();
+module.exports = fizzBuzz;
