05278fd7b1d22d92142e49788376538f9d334b97
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-15 15:17:42 +0100

    Update randomNumber.js

--- randomNumber.js
+++ randomNumber.js
@@ -4,4 +4,5 @@ function randomNumber() {
     let max = 10;
     console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 }
-module.exports = randomNumber();
+randomNumber();
+module.exports = randomNumber;
