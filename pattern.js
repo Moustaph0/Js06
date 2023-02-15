a4e829f82706a9a6409a62997d97452965c0b3b2
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-15 14:06:16 +0100

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -8,3 +8,16 @@ function pattern(){
 }
 module.exports = pattern();
 
+function Pyramide(n, c) { // n = niveaux hiérarchiques; c = caractère utilisé
+    // Nb de colonnes =  Nb d'espaces + nb de caractères "*"
+    // Chaque nouvelle ligne gagne 2 "*"
+    let nbColonnes = 1 + 2 * (n - 1), nbSpaceChar = nbColonnes - 1, output = "";
+    for (let lineNb = 0, nbTextChar = 1;
+        lineNb < n;
+        lineNb++, nbSpaceChar -= 2, nbTextChar += 2) {
+            output = output.concat(" ".repeat(nbSpaceChar)).concat(c.repeat(nbTextChar));
+            output = (lineNb < n - 1) ? output.concat("\n") : output;
+    }
+    return output;
+}
+console.log(Pyramide(4, "*"));
